f2b1e772ea8a35ed2390ba55db2391d4a391e3c7
lukasz Dechnik <lukasz@dechnik.net>
2023-07-24 21:06:25 +0200

    chmod ssh key after rsync

--- src/nixos-anywhere.sh
+++ src/nixos-anywhere.sh
@@ -437,6 +437,7 @@ if [[ -n ${copy_ssh-} ]]; then
   step Copying ssh key file
   ssh_ "mkdir -p /mnt/persist/etc/ssh"
   rsync -rlpv -FF -e "ssh -i \"$ssh_key_dir\"/nixos-anywhere -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" "${copy_ssh}" "${ssh_connection}:/mnt/persist/etc/ssh/"
+  ssh_ "chmod 400 /mnt/persist/etc/ssh/${copy_ssh}"
   rsync -rlpv -FF -e "ssh -i \"$ssh_key_dir\"/nixos-anywhere -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" "${copy_ssh}.pub" "${ssh_connection}:/mnt/persist/etc/ssh/"
 fi
 
